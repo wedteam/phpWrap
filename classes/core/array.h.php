d91233faedf8d6154de1e7496ab05d4e48cabc45
akira <akira.cn@gmail.com>
2011-12-07 21:58:37 +0800

    add comment

--- classes/core/array.h.php
+++ classes/core/array.h.php
@@ -1,6 +1,23 @@
 <?php
 require_once('helper.h.php');
+
+/**
+ * 数组的Helper方法
+ *
+ * @package    PhpWrap
+ * @category   Core
+ * @author     akira.cn@gmail.com
+ * @copyright  (c) 2011 WED Team
+ * @license    http://qwrap.com
+ */
 class ArrayH extends HelperH {
+	/**
+	 * 判断数组是一个传统的list还是一个关联数组
+	 * 这个方法来自 Kohana 3.2 
+	 * 
+	 * @param ArrayRef $array
+	 * @return boolean
+	 */
 	public static function is_assoc(array &$array)
 	{
 		// Keys of the array
@@ -20,9 +37,11 @@ class ArrayH extends HelperH {
 	 * "#a b" 查找唯一的a属性的子孙中包含所有b属性
 	 * "a>b" 查找a属性下的b属性儿子
 	 * "a+b" 查找兄弟，即同时有a、b属性，取b
-	 * "a b[value=1]" 查找并过滤value=1的结果
+	 *	
+     * @param ArrayRef $array
+	 * @param String $selector
 	 */
-	public static function query(array &$array, $selector, $byRef = false){
+	public static function query(array &$array, $selector){
 		$selector = preg_replace('/([#>])/',' ${1} ',$selector);
 		$selector = preg_replace('/\s*(\+)\s*/', '${1}', $selector);
 		$selector = explode(' ', $selector);
@@ -31,8 +50,9 @@ class ArrayH extends HelperH {
 		$pat = '';
 
 		foreach($selector as $token){
+			//对选择符从左到右依次进行处理
 			if($token == '') continue;
-
+			
 			if(in_array($token, array('#','>'))){
 				$pat = $token;
 			}
@@ -54,51 +74,70 @@ class ArrayH extends HelperH {
 		
 		return $ret;
 	}
+	/**
+	 * 寻找兄弟节点
+	 *
+	 * @param ArrayRef  $array
+	 * @param String $key
+	 * @param String $brother 兄弟节点
+	 * @param Boolean $find_one 只找一个
+	 * @param Boolean $child_only 只找直系儿子，不递归查找
+	 * @return Array 符合条件的结果数组
+	 */
 	private static function _searchBrotherKey(array &$array, $key, $brother, $find_one = false, $child_olny = false){
 		$key_match = null; $brother_match = null;
 		$ret = array();
-		if($child_only){
-			if(array_key_exists($key, $array) && array_key_exists($brother, $array)){
-				$ret[] = &$array[$key];
-			}
+		//如果找到了，返回
+		if(array_key_exists($key, $array) && array_key_exists($brother, $array)){
+			$ret[] = &$array[$key];
 		}
-		else{
+		else if(!$child_only){
+			//否则如果不只找儿子的话，需要递归往下查找
 			foreach($array as $k => &$v){
-				if($key === $k){
-					$key_match = &$v;
-				}else if($brother === $k){
-					$brother_match = $v;
-				}
-				if(isset($key_match) && isset($brother_match)){
-					$ret[] = &$key_match;
-				}else if(is_array($v)){
+				if(is_array($v)){
 					$ret = array_merge($ret, self::_searchBrotherKey($v, $key, $brother, $find_one));
 				}
-				if($find_one && count($ret) > 0) return $ret; //如果只找一个，找到就返回
+				if($find_one && count($ret) > 0) break; //如果只找一个，找到就返回
 			}
 		}
 		return $ret;
 	}
-
+	
+	/**
+	 * 寻找子孙节点
+	 *
+	 * @param ArrayRef  $array
+	 * @param String $key
+	 * @param Boolean $find_one 只找一个
+	 * @param Boolean $child_only 只找直系儿子，不递归查找
+	 * @return Array 符合条件的结果数组
+	 */
 	private static function _searchKey(array &$array, $key, $find_one = false, $child_only = false){
 		$ret = array();
-		if($child_only){
-			if(array_key_exists($key, $array)){
-				$ret[] = &$array[$key];
-			}
-		}else{
+
+		if(array_key_exists($key, $array)){
+			//如果找到了一个，不需要再找本系谱子孙
+			$ret[] = &$array[$key];
+		}
+		else if(!$child_only){
+			//否则如果不只找儿子的话，要继续往下查
 			foreach($array as $k => &$v){
-				if($key === $k){
-					$ret[] = &$v;
-				}else if(is_array($v)){
+				if(is_array($v)){
 					$ret = array_merge($ret, self::_searchKey($v, $key, $find_one));
 				}
-				if($find_one&& count($ret) > 0) return $ret; //如果只找一个，找到就返回
+				if($find_one && count($ret) > 0) break; //如果只找一个，找到就返回
 			}
 		}
 		return $ret;
 	}
-
+	
+	/**
+	 * 生成数组镜像，一个数组镜像的每一个key都是另一个数组的对应key的引用
+	 * 用来生成重要的数组拷贝引用，来关联core和Wrap
+	 *
+	 * @param ArrayRef $array
+	 * @return Array 镜像数组
+	 */
 	public static function mirror(array &$arr){
 		$ret = array();
 		foreach($arr as $k => $v){
@@ -107,6 +146,12 @@ class ArrayH extends HelperH {
 		return $ret;
 	}	
 	
+	/**
+	 * 遍历迭代数组，对每个元素执行定义的方法
+	 *
+	 * @param ArrayRef $array
+	 * @param Closure $callback
+	 */
 	public static function each(array &$array, $callback){
 		return array_map(
 			$callback, $array
--- classes/core/core.php
+++ classes/core/core.php
@@ -1,12 +1,42 @@
 <?php 
+/**
+ * Wrap是一个包装对象，它可以“修饰”php原生对象，Wrap修饰后的php原生对象有几个作用
+ * 首先是能够支持Helper链式调用和批量操作
+ * 所谓Helper链式调用是指能用Helper对象上的范式方法作为Wrap对象的方法来调用，并链式返回Wrap后的结果
+ * 批量操作是指Wrap对象可以包装一个数组，对数组上的每一个元素应用Helper对象的方法
+ * 其次，Helper可以将php原生函数符合Helper规范的方法也代理成链式调用方法，如substr、strlen等等
+ * Helper规范和Wrap的概念参考著名的JavaScript框架 QWrap
+ * 
+ * @package    PhpWrap
+ * @category   Core
+ * @author     akira.cn@gmail.com
+ * @copyright  (c) 2011 WED Team
+ * @license    http://qwrap.com
+ */
 class Wrap{
+	/**
+	 * 包装方法的核心对象，用引用方式关联非数组的宿主对象，用mirror方式关联数组
+	 *
+	 * @var mixed $core
+	 */
 	public $core;
-
+	
+	/**
+	 *
+	 * 配置映射表，出于效率考虑，只支持一个类型对应一个Helper，暂不支持用户自定义object细分类型对应Helper
+	 * @var Array $helper Helper
+	 */
 	protected static $helpers = array(
 		'string' => 'StringH',
 		'array'	=> 'ArrayH',
 	);
-
+	
+	/**
+	 * Wrap的自动类加载器
+	 *
+	 * @param String $class 要加载的类名
+	 * @return Boolean 是否加载成功
+	 */
 	public static function auto_load($class){
 		$class = strtolower(preg_replace("/([A-Z])$/", ".$1", $class));
 		try{
@@ -17,15 +47,13 @@ class Wrap{
 		}
 		
 	}
-
-	protected static function gettype($obj){
-		if(gettype($obj) != 'object'){
-			return gettype($obj);
-		}else{
-			return strtolower(get_class($obj));
-		}
-	}
 	
+	/**
+	 * 取出或设置Wrap中的元素中Key-Value内容，对于非数组，会抛出index异常
+	 *
+	 * @param String $key 
+	 * @param mixed $value 缺省是setter，传入该参数则作为setter
+	 */
 	public function item($key, $value = null){
 		if(isset($value)){
 			$this->core[$key] = $value;
@@ -35,85 +63,138 @@ class Wrap{
 			return new Wrap($this->core[$key]);
 		}
 	}
-
+	
 	public function __toString(){
 		return json_encode($this->core);
 	}
-
+	
+	/**
+	 * 构造一个新的Wrap，对于Helper有返回值的function调用后会构造新的Wrap，无返回值的function则会直接返回当前Wrap
+	 *
+	 * @param mixedRef $core 代理的数据对象
+	 */
 	function __construct(&$core){
 		$this->core = &$core;
 	}
 	
+	/**
+	 * 类型转为数组
+	 */
+	public function as_array(){
+		if(is_array($this->core))
+			return $this->core;
+		else
+			throw new Exception('type convert error');
+	}
+
+	/**
+	 * 类型转为字符串
+	 */
+	public function as_string(){
+		return $this->__toString();
+	}
+
+	/**
+	 * 类型转为整数
+	 */
+	public function as_int(){
+		return intval($this->core);
+	}
+	
+	/**
+	 * 类型转为浮点数
+	 */
+	public function as_float(){
+		return floatval($this->core);
+	}
+
+	/**
+	 * 类型转为布尔
+	 */
+	public function as_bool(){
+		return !!$this->core;
+	}
+	
+	/**
+	 * 任意类型，直接返回core
+	 */
+	public function as_mixed(){
+		return $this->core;
+	}
+
+	/**
+	 * 用魔术方法来代理Helper方法
+	 *
+	 * @param String $func 要代理的方法名
+	 * @param Array $args 方法调用的参数
+	 */
 	function __call($func, $args){
-		/*
-			"boolean"
-			"integer"
-			"double" 
-			"string"
-			"array"
-			"mixed"
-		 */
-		if(strpos($func, 'as_') === 0){
-			$ret = $this->core;
-			$type = gettype($ret);
-			if($func != 'as_mixed' && $type != substr($func, 3)){
-				if($func == 'as_string'){
-					return ''.$ret;
-				}else if($func == 'as_integer'){
-					return intval($ret);
-				}else if($func == 'as_double'){
-					return doubleval($ret);
-				}else if($func == 'as_boolean'){
-					return !!$ret;
-				}
-				throw new Exception('type convert error!');
-			}
-			return $ret;
-		}else{
-			array_unshift($args, null);
-			$args[0] = &$this->core;
-			if(is_array($args[0])  
-				&& !method_exists(self::$helpers['array'], $func)){ //区分数组？ query? W($data).query('>data')...
-				//不是 array_ 系列方法时，将array作为集合操作
-				$_r = array();
-				foreach($args[0] as &$core){
-					$helper = gettype($core);
-					if(method_exists(self::$helpers[$helper], $func)){
-						$func = array(self::$helpers[$helper],$func); 
-					}
-					$ret = call_user_func_array($func, array_merge(array($core),array_slice($args,1)));
-					if(isset($ret))
-						$_r[] = $ret;
-				}
-				return new Wrap($_r);
-			}else{
-				$helper = gettype($args[0]); 
+		// 将$this->core作为第一个参数
+		array_unshift($args, null);
+		$args[0] = &$this->core;
+		
+		if(is_array($args[0])  
+			&& !method_exists(self::$helpers['array'], $func))
+		{ 
+			//如果core是数组并且不是调用ArrayH上的方法
+			//将数组作为集合来进行批量操作
+			$_r = array();
+			foreach($args[0] as &$core){
+				$helper = gettype($core);
 				if(method_exists(self::$helpers[$helper], $func)){
-					$func = array(self::$helpers[$helper], $func);
+					$func = array(self::$helpers[$helper],$func); 
 				}
-				$ret = call_user_func_array($func, $args);
-				if(isset($ret)){
-					return new Wrap($ret);
-				}
+				$ret = call_user_func_array($func, array_merge(array($core),array_slice($args,1)));
+				if(isset($ret))
+					$_r[] = $ret;
 			}
-			return $this;
+			return new Wrap($_r);
+		}else{
+			$helper = gettype($args[0]); 
+			//如果存在对应的Helper并且Helper上有此方法
+			if(method_exists(self::$helpers[$helper], $func)){
+				$func = array(self::$helpers[$helper], $func);
+			}
+			//否则，尝试调用php系统自带的方法
+			$ret = call_user_func_array($func, $args);
+			if(isset($ret)){	//如果有返回值，用返回值的Wrapper代替$this
+				return new Wrap($ret);
+			}
 		}
+		return $this;
 	}
 }
 
+/**
+ * 如果是常量，用这个函数生成代理，因为默认代理必须是core的引用，直接调用常量无法引用
+ *
+ * @param const $constant
+ * @return Wrap
+ */
 function C($constant){
 	return W($constant);
 }
+
+/**
+ * 如果是变量，产生包装的Wrap对象
+ * 
+ * @param mixedRef $mixed
+ */
 function W(&$mixed){
+	//如果已经是Wrap对象，那么复制它
 	if($mixed instanceof Wrap){
 		$mixed = &$mixed->core;
 	}
+	//如果是数组，传数组拷贝引用
 	if(is_array($mixed)){
-		$rr = ArrayH::mirror($mixed); //如果是数组，传数组拷贝引用
+		$rr = ArrayH::mirror($mixed);
 	}else{
-		$rr = &$mixed;	//否则传引用
+		$rr = &$mixed;	//否则直接传变量引用
 	}
 	return new Wrap($rr);
 }
 
+/**
+ * 注册类自动加载器
+ */
 spl_autoload_register(array('Wrap', 'auto_load'));
