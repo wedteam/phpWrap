0383b60fb806c7d20ed70b3494861f6ec29a38cc
akira <akira.cn@gmail.com>
2011-12-07 18:22:24 +0800

    array ref system

--- classes/core/array.h.php
+++ classes/core/array.h.php
@@ -1,7 +1,7 @@
 <?php
 require_once('helper.h.php');
 class ArrayH extends HelperH {
-	public static function is_assoc(array $array)
+	public static function is_assoc(array &$array)
 	{
 		// Keys of the array
 		$keys = array_keys($array);
@@ -9,5 +9,93 @@ class ArrayH extends HelperH {
 		// If the array keys of the keys match the keys, then the array must
 		// not be associative (e.g. the keys array looked like {0:0, 1:1...}).
 		return array_keys($keys) !== $keys;
+	}
+
+	/**
+	 * 根据selector查询数组，并返回找到的查询结果
+	 *
+	 * 支持的查询格式有
+	 *
+	 * "a b" 查找a属性的子孙中包含b属性
+	 * "#a b" 查找唯一的a属性的子孙中包含所有b属性
+	 * "a>b" 查找a属性下的b属性儿子
+	 * "a+b" 查找兄弟，即同时有a、b属性，取b
+	 * "a b[value=1]" 查找并过滤value=1的结果
+	 */
+	public static function query(array &$array, $selector, $byRef = false){
+		$selector = preg_replace('/([#>])/',' ${1} ',$selector);
+		$selector = preg_replace('/\s*(\+)\s*/', '${1}', $selector);
+		$selector = explode(' ', $selector);
+		
+		$ret = array($array);
+		$pat = '';
+
+		foreach($selector as $token){
+			if($token == '') continue;
+
+			if(in_array($token, array('#','>'))){
+				$pat = $token;
+			}
+			else{
+				$_r = array(); //temp
+				foreach($ret as &$arr){
+					if(strpos($token, '+') !== false){
+						$k_b = explode('+', $token);
+						$_r += self::_searchBrotherKey($arr, $k_b[1], $k_b[0], $pat == '#', $pat == '>');
+					}
+					else{
+						$_r += self::_searchKey($arr, $token, $pat == '#', $pat == '>');
+					}
+				}
+				$ret = $_r;
+			}
+		}
+		
+		return $ret;
+	}
+	private static function _searchBrotherKey(array &$array, $key, $brother, $find_one = false, $child_olny = false){
+		$key_match = null; $brother_match = null;
+		$ret = array();
+		foreach($array as $k => &$v){
+			if($key === $k){
+				$key_match = &$v;
+			}else if($brother === $k){
+				$brother_match = $v;
+			}
+			if(isset($key_match) && isset($brother_match)){
+				$ret[] = &$key_match;
+			}else if(!$child_only && is_array($v)){
+				$ret = array_merge($ret, self::_searchBrotherKey($v, $key, $brother, $find_one));
+			}
+			if(($find_one || $child_only) && count($ret) > 0) return $ret; //如果只找一个，找到就返回
+		}
+		return $ret;
+	}
+
+	private static function _searchKey(array &$array, $key, $find_one = false, $child_only = false){
+		$ret = array();
+		foreach($array as $k => &$v){
+			if($key === $k){
+				$ret[] = &$v;
+			}else if(!$child_only && is_array($v)){
+				$ret = array_merge($ret, self::_searchKey($v, $key, $find_one));
+			}
+			if(($find_one || $child_only)&& count($ret) > 0) return $ret; //如果只找一个，找到就返回
+		}
+		return $ret;
+	}
+
+	public static function mirror(array &$arr){
+		$ret = array();
+		foreach($arr as $k => $v){
+			$ret[$k] = &$arr[$k];
+		}
+		return $ret;
+	}	
+	
+	public static function each(array &$array, $callback){
+		return array_map(
+			$callback, $array
+		);
 	}
 }
--- classes/core/core.php
+++ classes/core/core.php
@@ -1,11 +1,10 @@
 <?php 
 class Wrap{
 	public $core;
-	
-	protected $func_queue = array();
-	
+
 	protected static $helpers = array(
 		'string' => 'StringH',
+		'array'	=> 'ArrayH',
 	);
 
 	public static function auto_load($class){
@@ -26,11 +25,25 @@ class Wrap{
 			return strtolower(get_class($obj));
 		}
 	}
+	
+	public function item($key, $value = null){
+		if(isset($value)){
+			$this->core[$key] = $value;
+			return $this;
+		}
+		else{
+			return new Wrap($this->core[$key]);
+		}
+	}
+
+	public function __toString(){
+		return json_encode($this->core);
+	}
 
 	function __construct(&$core){
 		$this->core = &$core;
 	}
-
+	
 	function __call($func, $args){
 		/*
 			"boolean"
@@ -60,30 +73,47 @@ class Wrap{
 			array_unshift($args, null);
 			$args[0] = &$this->core;
 			if(is_array($args[0])  
-				&& !method_exists(self::$helpers['array'], $func)
-				&& !ArrayH::is_assoc($args[0])){ //区分数组？ query? W($data).query('>data')...
+				&& !method_exists(self::$helpers['array'], $func)){ //区分数组？ query? W($data).query('>data')...
 				//不是 array_ 系列方法时，将array作为集合操作
-				foreach($args[0] as $key => $core){
-					$helper = gettype($args[0][$key]);
+				$_r = array();
+				foreach($args[0] as &$core){
+					$helper = gettype($core);
 					if(method_exists(self::$helpers[$helper], $func)){
-						$func = array($helper,$func); 
+						$func = array(self::$helpers[$helper],$func); 
 					}
-					$args[0][$key] = call_user_func_array($func, array_merge(array($args[0][$key]),array_slice($args,1)));
+					$ret = call_user_func_array($func, array_merge(array($core),array_slice($args,1)));
+					if(isset($ret))
+						$_r[] = $ret;
 				}
+				return new Wrap($_r);
 			}else{
 				$helper = gettype($args[0]); 
 				if(method_exists(self::$helpers[$helper], $func)){
-					$func = array($helper, $func);
+					$func = array(self::$helpers[$helper], $func);
 				}
-				$args[0] = call_user_func_array($func, $args);
+				$ret = call_user_func_array($func, $args);
+				if(isset($ret)){
+					return new Wrap($ret);
+				}
 			}
 			return $this;
 		}
 	}
 }
 
-function W($mixed){
-	return new Wrap($mixed);
+function C($constant){
+	return W($constant);
+}
+function W(&$mixed){
+	if($mixed instanceof Wrap){
+		$mixed = &$mixed->core;
+	}
+	if(is_array($mixed)){
+		$rr = ArrayH::mirror($mixed); //如果是数组，传数组拷贝引用
+	}else{
+		$rr = &$mixed;	//否则传引用
+	}
+	return new Wrap($rr);
 }
 
 spl_autoload_register(array('Wrap', 'auto_load'));
